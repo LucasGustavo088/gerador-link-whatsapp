67b71072aff4feb56fd67ad54d3003df7e4385ec
Lucas Gustavo Barbosa <lucas.gustavo.barbosa@ibm.com>
2022-06-29 22:03:25 -0300

    feat: finalizado, aguardando ajustes e google tag manager

---
+++ whatsapp.js
@@ -0,0 +1,27 @@
+var generatedValue = '';
+
+$(document).ready(item => {
+    $("#generate-btn").click(() => {
+        generateLink();
+    });
+    $("#copy-to-clipboard").click(() => {
+        navigator.clipboard.writeText(generatedValue);
+        alert('Copiado! Aumente suas vendas...')
+    }); 
+});
+
+function generateLink() {
+    generatedValue = '';
+    let number = $("#number").val();
+    let message = $("#message").val();
+
+    if(number == null || message == null) {
+        alert("O campo número e mensagem não deve ser vazio");
+        return;
+    }
+
+    generatedValue = `https://api.whatsapp.com/send?phone=55${number}&text=${message}`;
+
+    $("#generated-value-text").html(generatedValue);
+    $("#generated-value").css("display", "block");
+}
